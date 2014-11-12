911bb4eaf32759f12e247215374e6ed094e06a9a
ochenmiller <peterfoxonmiller@gmail.com>
2014-11-12 11:56:02 -0500

    Create cal.sh

---
+++ cal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# dexterity.sh config file
+# calibration data
+SLOPE=$1
+INT=$2
+SCALE=$3
+echo "# dexterity.sh config file" > cal.cfg
+echo "# calibration data" >> cal.cfg
+echo "SLOPE=$1" >> cal.cfg
+echo "INT=$2"   >> cal.cfg
+echo "SCALE=$3"  >> cal.cfg
